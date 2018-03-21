6f9c8d38438311d5c4ea823863eafef44eb5768e
emalikturhan <emalikturhan@gmail.com>
2018-03-21 23:45:26 +0300

    home page with react

--- src/index.js
+++ src/index.js
@@ -1,8 +1,165 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+//import App from './App';
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+
+const inputParsers = {
+  date(input) {
+     const [month, day, year] =input.split('/');
+    return `${year}-${month}-${day}`;
+  },
+  uppercase(input) {
+    return input.toUpperCase();
+  },
+  number(input) {
+    return parseFloat(input);
+  },
+};
+
+class ShakingError extends React.Component {
+	constructor() { 
+  super(); 
+  this.state = { key: 0 }; 
+  }
+
+	componentWillReceiveProps() {
+    // update key to remount the component to rerun the animation
+  	this.setState({ key: ++this.state.key });
+  }
+  
+  render() {
+  	return <div key={this.state.key} className="bounce">{this.props.text}</div>;
+  }
+}
+
+class MyForm extends React.Component {
+  constructor() {
+    super();
+    this.state = {};
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleSubmit(event) {
+    event.preventDefault();
+    if (!event.target.checkValidity()) {
+    	this.setState({
+        invalid: true,
+        displayErrors: true,
+      });
+      return;
+    }
+    const form = event.target;
+    const data = new FormData(form);
+
+    for (let name of data.keys()) {
+      const input = form.elements[name];
+      const parserName = input.dataset.parse;
+      console.log('parser name is', parserName);
+      if (parserName) {
+        const parsedValue = inputParsers[parserName](data.get(name))
+        data.set(name, parsedValue);
+      }
+    }
+    
+    this.setState({
+    	res: stringifyFormData(data),
+      invalid: false,
+      displayErrors: false,
+    });
+
+ /*   fetch('/api/form-submit-url', {
+      method: 'POST',
+       body: data,
+   });*/
+  }
+
+  render() {
+  	const { res, invalid, displayErrors } = this.state;
+    return (
+    	<div>
+        <form
+          onSubmit={this.handleSubmit}
+          noValidate
+          className={displayErrors ? 'displayErrors' : ''}
+         >
+          <label htmlFor="location">Location:</label>
+          <input
+            id="location"
+            name="location"
+            type="text"
+            data-parse="uppercase"
+            required
+          />
+
+          <label htmlFor="datein">Date-in:</label>
+          <input
+            id="datein"
+            name="datein"
+            type="text"
+            data-parse="date"
+            placeholder="MM/DD//YYYY"
+            pattern="\d{2}\/\d{2}/\d{4}"
+            required
+          />
+          
+           <label htmlFor="dateout">Date-out:</label>
+          <input
+            id="dateout"
+            name="dateout"
+            type="text"
+            data-parse="date"
+            placeholder="MM/DD//YYYY"
+            pattern="\d{2}\/\d{2}/\d{4}"
+            required
+          />
+          
+         
+          <label htmlFor="guests">Guests:</label>
+          <input
+            id="guests"
+            name="guests"
+            type="number" min="1"
+            data-parse="number"
+            placeholder="1"           
+            required
+          />
+          
+          
+          <button>Search</button>
+        </form>
+        
+        
+        
+        <div className="res-block">
+          {invalid && (
+            <ShakingError text="Form is not valid" />
+          )}
+          {!invalid && res && (
+          	<div>
+              <h3>Transformed data to be sent:</h3>
+              <pre>FormData {res}</pre>
+          	</div>
+          )}
+        </div>
+    	</div>
+    );
+  }
+}
+
+ReactDOM.render(
+	<MyForm />,
+  document.getElementById("root")
+);
+
+
+function stringifyFormData(fd) {
+  const data = {};
+	for (let key of fd.keys()) {
+  	data[key] = fd.get(key);
+  }
+  return JSON.stringify(data, null, 2);
+}
+
+//ReactDOM.render(<App />, document.getElementById('root'));
+
